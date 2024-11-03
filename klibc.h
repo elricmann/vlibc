034d3368c6ac4cf85b40fd925809368bf95aafa8
elricmann <elricmann@proton.me>
2024-11-03 19:17:28 +0300

    feat: stdint

--- klibc.h
+++ klibc.h
@@ -21,3 +21,103 @@
 // SOFTWARE.
 
 #pragma once
+
+// KLIBC_STDINT_H
+
+#define KLIBC_INT8_MIN -128
+#define KLIBC_INT8_MAX 127
+#define KLIBC_UINT8_MAX 255
+
+#define KLIBC_INT16_MIN -32768
+#define KLIBC_INT16_MAX 32767
+#define KLIBC_UINT16_MAX 65535
+
+#define KLIBC_INT32_MIN -2147483648
+#define KLIBC_INT32_MAX 2147483647
+#define KLIBC_UINT32_MAX 4294967295U
+
+#define KLIBC_INT64_MIN -9223372036854775808LL
+#define KLIBC_INT64_MAX 9223372036854775807LL
+#define KLIBC_UINT64_MAX 18446744073709551615ULL
+
+#define KLIBC_INT_FAST8_MIN KLIBC_INT8_MIN
+#define KLIBC_INT_FAST8_MAX KLIBC_INT8_MAX
+#define KLIBC_UINT_FAST8_MAX KLIBC_UINT8_MAX
+
+#define KLIBC_INT_FAST16_MIN KLIBC_INT16_MIN
+#define KLIBC_INT_FAST16_MAX KLIBC_INT16_MAX
+#define KLIBC_UINT_FAST16_MAX KLIBC_UINT16_MAX
+
+#define KLIBC_INT_FAST32_MIN KLIBC_INT32_MIN
+#define KLIBC_INT_FAST32_MAX KLIBC_INT32_MAX
+#define KLIBC_UINT_FAST32_MAX KLIBC_UINT32_MAX
+
+#define KLIBC_INT_FAST64_MIN KLIBC_INT64_MIN
+#define KLIBC_INT_FAST64_MAX KLIBC_INT64_MAX
+#define KLIBC_UINT_FAST64_MAX KLIBC_UINT64_MAX
+
+#define KLIBC_INT_LEAST8_MIN KLIBC_INT8_MIN
+#define KLIBC_INT_LEAST8_MAX KLIBC_INT8_MAX
+#define KLIBC_UINT_LEAST8_MAX KLIBC_UINT8_MAX
+
+#define KLIBC_INT_LEAST16_MIN KLIBC_INT16_MIN
+#define KLIBC_INT_LEAST16_MAX KLIBC_INT16_MAX
+#define KLIBC_UINT_LEAST16_MAX KLIBC_UINT16_MAX
+
+#define KLIBC_INT_LEAST32_MIN KLIBC_INT32_MIN
+#define KLIBC_INT_LEAST32_MAX KLIBC_INT32_MAX
+#define KLIBC_UINT_LEAST32_MAX KLIBC_UINT32_MAX
+
+#define KLIBC_INT_LEAST64_MIN KLIBC_INT64_MIN
+#define KLIBC_INT_LEAST64_MAX KLIBC_INT64_MAX
+#define KLIBC_UINT_LEAST64_MAX KLIBC_UINT64_MAX
+
+#define KLIBC_INTMAX_MIN KLIBC_INT64_MIN
+#define KLIBC_INTMAX_MAX KLIBC_INT64_MAX
+#define KLIBC_UINTMAX_MAX KLIBC_UINT64_MAX
+
+#define KLIBC_INTPTR_MIN KLIBC_INT64_MIN
+#define KLIBC_INTPTR_MAX KLIBC_INT64_MAX
+#define KLIBC_UINTPTR_MAX KLIBC_UINT64_MAX
+
+#define klibc_int8_t signed char
+#define klibc_uint8_t unsigned char
+
+#define klibc_int16_t signed short
+#define klibc_uint16_t unsigned short
+
+typedef signed int klibc_int32_t;
+typedef unsigned int klibc_uint32_t;
+
+typedef signed long long klibc_int64_t;
+typedef unsigned long long klibc_uint64_t;
+
+typedef signed char klibc_int_fast8_t;
+typedef unsigned char klibc_uint_fast8_t;
+
+typedef signed short klibc_int_fast16_t;
+typedef unsigned short klibc_uint_fast16_t;
+
+typedef signed int klibc_int_fast32_t;
+typedef unsigned int klibc_uint_fast32_t;
+
+typedef signed long long klibc_int_fast64_t;
+typedef unsigned long long klibc_uint_fast64_t;
+
+typedef signed char klibc_int_least8_t;
+typedef unsigned char klibc_uint_least8_t;
+
+typedef signed short klibc_int_least16_t;
+typedef unsigned short klibc_uint_least16_t;
+
+typedef signed int klibc_int_least32_t;
+typedef unsigned int klibc_uint_least32_t;
+
+typedef signed long long klibc_int_least64_t;
+typedef unsigned long long klibc_uint_least64_t;
+
+typedef signed long long klibc_intmax_t;
+typedef unsigned long long klibc_uintmax_t;
+
+typedef signed long long klibc_intptr_t;
+typedef unsigned long long klibc_uintptr_t;
